72c21869c15db0fa84de25b214d442bbdc7c2005
Fullgonzo <jay@zap-media.ca>
2026-04-20 01:32:42 -0400

    Delete mod_ciwv_radiocharts directory

--- mod_ciwv_radiocharts/sql/uninstall.mysql.utf8.sql
+++
@@ -1,6 +0,0 @@
--- CIWV Radiocharts: uninstall.mysql.utf8.sql
--- WARNING: This script is NOT executed automatically by the module.
--- The database is managed manually. Run this only if you want to
--- permanently delete all stored chart history.
-
-DROP TABLE IF EXISTS `d6f25_ciwv_radiocharts`;
